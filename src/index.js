9ec3bee9e6b2c5f2e1dd342fffb732cc391b19ab
volshibenik <volshibenik@gmail.com>
2017-09-17 22:00:18 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
 }
+
+
+
+//http://www.securitylab.ru/blog/personal/aguryanov/30030.php
+
+//https://ru.wikipedia.org/wiki/%D0%94%D0%BB%D0%B8%D0%BD%D0%BD%D0%B0%D1%8F_%D0%B0%D1%80%D0%B8%D1%84%D0%BC%D0%B5%D1%82%D0%B8%D0%BA%D0%B0
