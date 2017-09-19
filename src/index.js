ec62fb76a142e9dc9bcbdf20a3df027f84ccaf10
Sergey S <volshibenik@gmail.com>
2017-09-19 16:58:01 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
 module.exports = function multiply(first, second) {
 
+	var arr1 = arrify(first),
+			arr2 = arrify(second),
+			resultArr = [];
+
+	for (var l = 0; l < resultLength(arr1,arr2); l++) {
+		resultArr[l] = 0;
+	}
+
+	var product = 0,
+			memo = 0,
+			memo2 = 0;
+
+	for (var i = arr1.length-1; i >= 0; i--) {
+
+		for (var j = arr2.length-1; j >= 0; j--) {
+
+			product = arr1[i]*arr2[j];
+			product += memo;
+
+			if (product >= 10) {
+
+				memo = Math.floor(product/10);
+				product -= (memo*10);
+
+			}	else 	{memo = 0;}
+
+			resultArr[i+j]+= product;
+
+			if (resultArr[i+j]>=10) {
+
+				memo2=Math.floor(resultArr[i+j]/10);
+				resultArr[i+j] -= memo2*10;
+				memo+=memo2;
+
+			}
+
+			if (j==0 && memo !==0) {
+
+				if (i===0) {
+
+					resultArr.unshift(memo);
+					memo = 0;
+					resultArr.pop();
+
+				}else {
+
+					resultArr[i+j-1]+=memo;
+					memo=0;
+
+				}
+			}
+		}
+	}
+	return resultArr.join('');
 }
 
 
+function arrify (str) {
+	
+	var arr = [];
+	
+	for (var i = 0; i < str.length; i++) {
+		
+		arr.push(str[i]);
+		
+	}
+	return arr;
+}
 
+function resultLength(arr1,arr2) {
+	var i = 1;
 
-//http://www.securitylab.ru/blog/personal/aguryanov/30030.php
+	while(i<5) {
 
-//https://ru.wikipedia.org/wiki/%D0%94%D0%BB%D0%B8%D0%BD%D0%BD%D0%B0%D1%8F_%D0%B0%D1%80%D0%B8%D1%84%D0%BC%D0%B5%D1%82%D0%B8%D0%BA%D0%B0
+		var firstProd = arr1.join('').slice(0,i) * arr2.join('').slice(0,i),
+				powerP = Math.pow(10,(2*i-1)); 
+
+		if (firstProd < (powerP - powerP/10)) {
+
+			if (i === 1 && firstProd >5) {
+				i++
+			} else 	return arr1.length+arr2.length - 1;
+
+		}else if (firstProd > (powerP - powerP/10) && firstProd < powerP) {
+
+			i++;
+
+		}else if (firstProd >= powerP)  {
+
+			return arr1.length+arr2.length;
+
+		}
+
+	}
+	return arr1.length+arr2.length - 1;
+}
